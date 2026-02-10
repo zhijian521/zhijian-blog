b35f95a1422c2baf05b9c0f88f673db45b24e2cc
Yevin <yuwb0521@outlook.com>
2026-02-10 09:29:19 +0800

    feat: update site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 ﻿// Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '知见';
-export const SITE_DESCRIPTION = '一份简约、清晰的个人博客。';
+export const SITE_TITLE = 'Yewen Home';
+export const SITE_DESCRIPTION = '认真生活，简单做人，用心做事。';
